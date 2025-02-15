a26cb4854b210affbedd7d625af7af41306111be
tockri <trickster.mf@gmail.com>
2025-02-16 02:35:21 +0900

    materialdesignicons

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Vuetify
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
